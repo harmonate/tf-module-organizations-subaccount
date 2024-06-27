f74390746f34ac119eb76716aa252bd287eafc08
mptsolutioning <167113569+mptsolutioning@users.noreply.github.com>
2024-06-27 10:52:25 -0700

    v1.5

--- s3.tf
+++ s3.tf
@@ -70,22 +70,22 @@ resource "aws_s3_bucket_policy" "config_bucket_policy" {
     Version = "2012-10-17",
     Statement = [
       {
-        Sid       = "AWSConfigAclCheck20150319",
-        Effect    = "Allow",
+        Sid    = "AWSConfigAclCheck20150319",
+        Effect = "Allow",
         Principal = {
           Service = "config.amazonaws.com"
         },
-        Action    = "s3:GetBucketAcl",
-        Resource  = "${aws_s3_bucket.config_bucket.arn}"
+        Action   = "s3:GetBucketAcl",
+        Resource = "${aws_s3_bucket.config_bucket.arn}"
       },
       {
-        Sid       = "AWSConfigWrite20150319",
-        Effect    = "Allow",
+        Sid    = "AWSConfigWrite20150319",
+        Effect = "Allow",
         Principal = {
           Service = "config.amazonaws.com"
         },
-        Action    = "s3:PutObject",
-        Resource  = "${aws_s3_bucket.config_bucket.arn}/*",
+        Action   = "s3:PutObject",
+        Resource = "${aws_s3_bucket.config_bucket.arn}/*",
         Condition = {
           StringEquals = {
             "s3:x-amz-acl" = "bucket-owner-full-control"
@@ -157,22 +157,22 @@ resource "aws_s3_bucket_policy" "cloudtrail_bucket_policy" {
     Version = "2012-10-17",
     Statement = [
       {
-        Sid       = "AWSCloudTrailAclCheck20150319",
-        Effect    = "Allow",
+        Sid    = "AWSCloudTrailAclCheck20150319",
+        Effect = "Allow",
         Principal = {
           Service = "cloudtrail.amazonaws.com"
         },
-        Action    = "s3:GetBucketAcl",
-        Resource  = "${aws_s3_bucket.cloudtrail_bucket.arn}"
+        Action   = "s3:GetBucketAcl",
+        Resource = "${aws_s3_bucket.cloudtrail_bucket.arn}"
       },
       {
-        Sid       = "AWSCloudTrailWrite20150319",
-        Effect    = "Allow",
+        Sid    = "AWSCloudTrailWrite20150319",
+        Effect = "Allow",
         Principal = {
           Service = "cloudtrail.amazonaws.com"
         },
-        Action    = "s3:PutObject",
-        Resource  = "${aws_s3_bucket.cloudtrail_bucket.arn}/*",
+        Action   = "s3:PutObject",
+        Resource = "${aws_s3_bucket.cloudtrail_bucket.arn}/*",
         Condition = {
           StringEquals = {
             "s3:x-amz-acl" = "bucket-owner-full-control"
@@ -217,5 +217,8 @@ resource "aws_iam_policy" "s3_permissions_policy" {
 resource "aws_iam_role_policy_attachment" "attach_s3_permissions" {
   role       = aws_iam_role.config_role.name
   policy_arn = aws_iam_policy.s3_permissions_policy.arn
+  depends_on = [
+    aws_iam_role.config_role
+  ]
 }
 
